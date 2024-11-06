39131e3e7f79a86d52347ed44cbdb9e5e733c129
Jay Hirsch <jay.softwaredev@gmail.com>
2024-11-06 11:14:29 -0800

    wrap component inside LxProvider to provide context for useLexicon

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import './App.css'
 import ostrich from './assets/noun-ostrich-2511897.svg'
 import app_json from './App.json'
 
+interface DemoComponentProps {
+  admin: boolean
+}
+
 function App() {
   const [admin, setAdmin] = useState(false)
+  const searchParams = new URLSearchParams(window.location.search);
 
   useEffect(() => {
     if (searchParams.get('logout') === 'true' && admin === true) {
@@ -17,27 +22,35 @@ function App() {
       setAdmin(true)
     }
   }, [admin])
-  const lexicon = useLexicon(app_json)
-  const searchParams = new URLSearchParams(window.location.search);
   return (
     <div className="App">
       <LxProvider apiUpdateUrl="http://example.com/update">
-        <div>
-          <a href="https://thenounproject.com/creator/joerivera2000/">
-            <img className="logo" src={ostrich} />
-          </a>
-        </div>
-        <h1> { lexicon.get('title') } </h1>
-        <p> { lexicon.get('blurb', {product_name: lexicon.get('product_name')} ) } </p>
-        <p>{
-            admin ?
-              <a href="/?logout=true">view as non-administrator</a>
-                :
-              <a href="/?lexiconServerToken=test">demo as administrator</a>
-            } 
-        </p>
+        <DemoComponent admin={admin}>
 
+        </DemoComponent>
       </LxProvider>
+    </div>
+  )
+}
+
+function DemoComponent({admin}: DemoComponentProps) {
+  const lexicon = useLexicon(app_json)
+  return (
+    <div className="demo-component">
+      <div>
+        <a href="https://thenounproject.com/creator/joerivera2000/">
+          <img className="logo" src={ostrich} />
+        </a>
+      </div>
+      <h1> { lexicon.get('title') } </h1>
+      <p> { lexicon.get('blurb', {product_name: lexicon.get('product_name')} ) } </p>
+      <p>{
+          admin ?
+            <a href="/?logout=true">view as non-administrator</a>
+              :
+            <a href="/?lexiconServerToken=test">demo as administrator</a>
+          } 
+      </p>
     </div>
   )
 }
