13dad5eb5efad050f23c9f8592108cd09167e752
Jay Hirsch <jay.softwaredev@gmail.com>
2024-11-06 10:55:19 -0800

    add button to simulate login

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { LxProvider, useLexicon } from '@nitidbit/lexicon'
 import './App.css'
 
@@ -5,7 +6,19 @@ import ostrich from './assets/noun-ostrich-2511897.svg'
 import app_json from './App.json'
 
 function App() {
+  const [admin, setAdmin] = useState(false)
+
+  useEffect(() => {
+    if (searchParams.get('logout') === 'true' && admin === true) {
+      setAdmin(false)
+      sessionStorage.removeItem("lexiconServerToken")
+    }
+    if (sessionStorage.lexiconServerToken === 'test' && admin === false) {
+      setAdmin(true)
+    }
+  }, [admin])
   const lexicon = useLexicon(app_json)
+  const searchParams = new URLSearchParams(window.location.search);
   return (
     <div className="App">
       <LxProvider apiUpdateUrl="http://example.com/update">
@@ -16,6 +29,14 @@ function App() {
         </div>
         <h1> { lexicon.get('title') } </h1>
         <p> { lexicon.get('blurb', {product_name: lexicon.get('product_name')} ) } </p>
+        <p>{
+            admin ?
+              <a href="/?logout=true">view as non-administrator</a>
+                :
+              <a href="/?lexiconServerToken=test">demo as administrator</a>
+            } 
+        </p>
+
       </LxProvider>
     </div>
   )
